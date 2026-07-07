3bcd4dd9083cb2460b00c481ac4013ad38b7ed9d
berkahhariini999-rgb <berkahhariini999@gmail.com>
2026-07-07 14:12:45 +0700

    Initial Commit

---
+++ WalletUIApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalletUIApp
+//
+//  Created by Iqbal Alhadad on 07/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalletUIApp/WalletUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletUIAppApp.swift
+//  WalletUIApp
+//
+//  Created by Iqbal Alhadad on 07/07/26.
+//
+
+import SwiftUI
+
+@main
+struct WalletUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
